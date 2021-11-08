74a090a0d6e8e9aef43f6d89bbd49da6bbb46c47
ccarlos19 <57632969+ccarlos19@users.noreply.github.com>
2021-11-07 21:21:28 -0500

    Vita CRUD

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from './services/usuario/usuario.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+
+  usuarioForm!: FormGroup;
+
+  constructor(
+    public fb: FormBuilder,
+    public usuarioService: UsuarioService
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.usuarioForm = this.fb.group({
+      clave: ['', Validators.required],
+      edad: ['', Validators.required],
+      fechaNacimiento: ['', Validators.required],
+      nombre: ['', Validators.required],
+      rol: ['', Validators.required],
+    })
+  }
+
+  guardar(): void {
+    this.usuarioService.saveUser(this.usuarioForm.value).subscribe(resp => {
+
+    },
+      error => { console.error(error) }
+
+    )
+  }
+
+
+
+
+
 }
---
+++ Frontend/src/app/services/usuario/usuario.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, ɵHttpInterceptingHandler } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+   private API_SERVER = "http://localhost:8080/usuario";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAlluser(): Observable<any>{
+    return this.httpClient.get(this.API_SERVER);
+  }
+
+  public saveUser(usuario:any): Observable<any>{
+    return this.httpClient.post(this.API_SERVER,usuario);
+  }
+
+
+
+
+}
