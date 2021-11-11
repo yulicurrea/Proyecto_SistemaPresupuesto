224684e5100e47b996226c3c54425e06667e04b1
ccarlos19 <ccabrerap@unbosque.edu.co>
2021-11-11 16:48:13 -0500

    Cambios

--- Frontend/src/app/Components/login/login.component.ts
+++ Frontend/src/app/Components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/services/usuario/usuarios.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  cargando = false;
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar, private router: Router,public _usuarioServices: UsuariosService) {
+    this.form = this.fb.group({
+      usuario: ['', Validators.required],
+      clave: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  ingresar() {
+    this._usuarioServices.login(this.form.value).subscribe(resp =>{
+      if(resp===true){
+        this.carga();
+        this.router.navigate(['sistemapresupuesto']);
+      }else {
+        this.error()
+      }
+    })
+  }
+  error() {
+    this._snackBar.open('Usuario o contraseña ingresados son invalidos.', '', {
+
+      duration: 5000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+
+    });
+  }
+  carga() {
+    this.cargando = true;
+    setTimeout(() => {
+
+      this.cargando = false;
+
+    }, 1500);
+  }
 }
