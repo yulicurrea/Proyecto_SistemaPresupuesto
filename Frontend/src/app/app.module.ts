82ad00948b151e7163a337dbf4c8e7f25185c575
ccarlos19 <ccabrerap@unbosque.edu.co>
2021-11-10 23:17:56 -0500

    Login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import{ BrowserAnimationsModule } from'@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
+import { LoginComponent } from './Components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
